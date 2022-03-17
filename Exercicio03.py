79935471eb3d1212f854077789ea0f22bee459b8
Victor Fernandes <62028219+Victorfds-hub@users.noreply.github.com>
2022-03-17 19:26:18 -0300

    Create Exercicio03.py

---
+++ Exercicio03.py
@@ -0,0 +1,8 @@
+funcionario = int(input(""))
+horas = int(input(""))
+valor = float(input(""))
+
+salario = horas * valor 
+
+print("NUMBER = ", funcionario)
+print("SALARY = U$ {:.2f}" .format(salario))
